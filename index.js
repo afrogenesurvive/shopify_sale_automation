2b09bdd1e6fb2f19b2eb54084295b465fc8629f5
AfroGeneSurvive <52981035+afrogenesurvive@users.noreply.github.com>
2023-06-07 17:23:26 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// Import the Shopify library.
+const Shopify = require('shopify');
+
+const shopifyAccessToken = process.env.SHOPIFY_ACCESS_TOKEN;
+// Create a new Shopify client.
+const client = new Shopify({
+  accessToken: shopifyAccessToken,
+});
+
+// Get the current shop.
+const shop = await client.getShop();
+
+// Subscribe to the item_added event.
+Shopify.Events.on('item_added', itemAdded);
+
+// Function to handle the item added event.
+function itemAdded(event) {
+  // Get the item that was added to the order.
+  var item = event.data.item;
+
+  // Check if the item has a price of $0.00.
+  if (item.price == 0) {
+    // Set the maximum number of free items allowed in the order.
+    var maxFreeItems = event.data.paidItems * 2;
+
+    // Check if the number of free items in the order is greater than the maximum allowed.
+    if (event.data.freeItems > maxFreeItems) {
+      // Throw an error.
+      throw new Error("The maximum number of free items has been exceeded.");
+    } else {
+      // Show the user the error.
+      alert("The maximum number of free items has been exceeded.");
+    }
+  }
+}
+
+// Do something with the shop.
+console.log(shop);
