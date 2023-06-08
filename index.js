85e3684c5c50bcb73c4a3d761ac4d2ba634dd2cb
AfroGeneSurvive <52981035+afrogenesurvive@users.noreply.github.com>
2023-06-07 19:53:24 -0500

    kkk

--- index.js
+++ index.js
@@ -1,92 +1,25 @@
-// // Import the Shopify library.
-// const { log } = require('console');
-// const Shopify = require('shopify');
+import express from 'express';
+import ShopifyAPI from"@shopify/shopify-api";
 
-// const shopifyAccessToken = process.env.SHOPIFY_ACCESS_TOKEN;
-// // Create a new Shopify client.
-// const client = new Shopify({
-//   accessToken: shopifyAccessToken,
-// });
+const client = new ShopifyAPI({
+  shopUrl: "https://shopthriftnature.com/",
+  accessToken: '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'
+});
 
-// // Get the current shop.
-// const shop = await client.getShop();
+const app = express();
 
-// // Subscribe to the item_added event.
-// Shopify.Events.on('item_added', itemAdded);
-
-// // Function to handle the item added event.
-// function itemAdded(event) {
-//   // Get the item that was added to the order.
-//   var item = event.data.item;
-
-//   // Check if the item has a price of $0.00.
-//   if (item.price == 0) {
-//     // Set the maximum number of free items allowed in the order.
-//     var maxFreeItems = event.data.paidItems * 2;
-
-//     // Check if the number of free items in the order is greater than the maximum allowed.
-//     if (event.data.freeItems > maxFreeItems) {
-//       // Throw an error.
-//     //   throw new Error("The maximum number of free items has been exceeded.");
-//         const eventPayload = {
-//             message: "The maximum number of free items has been exceeded.",
-//         };
-      
-//       // Trigger a custom Shopify event named "custom_error_event".
-//         Shopify.Events.trigger('custom_error_event', eventPayload);
-
-//     } else {
-//       // Show the user the error.
-//     //   alert("The maximum number of free items has been exceeded.");
-//         console.log("order ok",event.data);
-//     }
-//   }
-// }
-
-// // Do something with the shop.
-// console.log(shop);
-
-
-
-// Import the Shopify SDK
-import shopify from 'shopify-buy';
-
-// Set the accessToken property
-shopify.accessToken = process.env.SHOPIFY_ACCESS_TOKEN;
-
-// Get the products
-async function getProducts() {
-    try {
-      const products = await shopify.getProducts();
+async function checkFreeItems(orderId) {
+    const order = await client.getOrder(orderId);
+    const freeItems = order.lineItems.filter((lineItem) => lineItem.price === 0);
+    const maxFreeItems = freeItems.length * 2;
   
-      // Render the products
-      for (const product of products) {
-        const productDiv = document.createElement('div');
-        productDiv.innerHTML = product.name;
-        document.getElementById('products').appendChild(productDiv);
-      }
-    } catch (error) {
-      console.log(error);
+    if (freeItems.length === maxFreeItems) {
+        client.flashError("You have reached the maximum number of free items.");
+    } else {
+      // Do nothing.
     }
   }
   
-  // Listen for an item add to order event
-  shopify.on('itemAddedToOrder', async (event) => {
-    // Check if the item is free
-    if (event.product.price === 0) {
-      // Check if the max free item amount has been reached
-      const maxFreeItemCount = event.order.items.filter(item => item.price > 0).length * 2;
-      if (event.order.freeItems.length >= maxFreeItemCount) {
-        // Show the customer an error
-        shopify.renderError('The maximum number of free items has been reached.', 500);
-      } else {
-        // Do nothing
-      }
-    } else {
-      // Do nothing
-    }
-  });
-  
-  // Get the products
-  getProducts();
+  // Listen for the `order:updated` event and call `checkFreeItems()` when it is fired.
+  client.on("order:updated", checkFreeItems);
   
